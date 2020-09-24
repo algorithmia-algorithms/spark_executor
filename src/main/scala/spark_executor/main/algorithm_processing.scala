a64c7a6eb2eeadec889e1e0d311be30d2829720f
James Sutton <james@algorithmia.com>
2020-09-24 12:25:02 -0500

    removed API key (now deleted)

--- src/main/scala/spark_executor/main/algorithm_processing.scala
+++ src/main/scala/spark_executor/main/algorithm_processing.scala
@@ -1,6 +1,6 @@
 package spark_executor.main
 
-import org.apache.spark.streaming.{Milliseconds, Seconds, StreamingContext}
+import org.apache.spark.streaming.{Milliseconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.streaming.dstream.DStream
 import play.api.libs.json._
@@ -8,7 +8,6 @@ import spark_executor.main.models.algorithmia.{AlgoInput, AlgoOutput}
 import com.algorithmia._
 import com.algorithmia.algo.{AlgoFailure, AlgoSuccess}
 import org.apache.spark.rdd.RDD
-
 import scala.collection.mutable
 
 object algorithm_processing {
@@ -17,7 +16,7 @@ object algorithm_processing {
   val CLIENT: AlgorithmiaClient = Algorithmia.client()
   val CONF: SparkConf = new SparkConf().setMaster("local[1]").setAppName("NetworkAnomalyDetector")
   val SC = new SparkContext(CONF)
-  val SSC = new StreamingContext(SC, Milliseconds(100))
+  val SSC = new StreamingContext(SC, Milliseconds(1000))
 
   def algorithm_request(input: AlgoInput, client: AlgorithmiaClient): AlgoOutput = {
     val result = client.algo(ALGO_NAME).pipe(input)
@@ -34,7 +33,7 @@ object algorithm_processing {
     // lets convert this RDD into a Stream that wrpe can use for processing
     val inputQueue: mutable.Queue[RDD[String]] = mutable.Queue()
     inputQueue += inputData
-    val inputStream = SSC.queueStream(inputQueue, oneAtATime = true)
+    val inputStream: DStream[String] = SSC.queueStream(inputQueue, oneAtATime = true)
 
     // Now that we have the stream loaded, lets convert it from a list of strings into our actual algorithm input format
     val algorithmInputs: DStream[AlgoInput] = inputStream
@@ -42,13 +41,15 @@ object algorithm_processing {
     // And lets call our orchestrator algorithm on each input in our DStream
     val algorithmOutputs: DStream[AlgoOutput] = algorithmInputs
       .map(req => algorithm_request(req, CLIENT))
-    var i = 0
+    // Now lets print the results to console as a basic Sink, this could be stored to disk as well.
+    // If our stream runs out of data, let's kill the stream and terminate the process.
+
     algorithmOutputs.foreachRDD(rdd => {
           if (rdd.isEmpty()) {
             Console.println("terminated job")
             SSC.stop(stopSparkContext = true, stopGracefully = false)
           } else {
-            rdd.foreach(Console.println)
+            rdd.foreach(l => {Console.println(Json.toJson(l))})
           }})
     SSC.start()
     SSC.awaitTermination()
